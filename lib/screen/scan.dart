32883314e1cc794708fd4d344553eaa157a6cc12
Apinya0 <apinya61145@gmail.com>
2024-04-05 19:54:54 +0700

    delect scan qr code

--- lib/screen/scan.dart
+++ lib/screen/scan.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:paddy_rice/constants/color.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 @RoutePage()
 class ScanRoute extends StatelessWidget {
